fa8bc400c2a0ad43e6d3de8d5bd8eeb2ed8b9635
Sefa Bulut <34122973+sefabulutt@users.noreply.github.com>
2023-04-02 18:11:19 +0300

    created IDisplayEntity on Entities Abstract

---
+++ Entities/Abstract/IDisplayEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    public interface IDisplayEntity
+    {
+        int DisplayOrder { get; set; }
+        bool IsDisplay { get; set; }
+    }
+}
