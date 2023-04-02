ed66df95e1a29129da751c4a61b00ba8d816f268
Sefa Bulut <34122973+sefabulutt@users.noreply.github.com>
2023-04-02 18:09:55 +0300

    created ICreatedEntity on Entities Abstract

---
+++ Entities/Abstract/ICreatedEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    public interface ICreatedEntity
+    {
+        int CreatedUserId { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
